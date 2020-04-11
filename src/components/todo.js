5d3642b1f53d2fb43cabeef6cf3eb72eb077746f
amin havasi <pau.ahq@gmail.com>
2020-04-11 10:35:01 +0430

    dynamic show

--- src/components/todo.js
+++ src/components/todo.js
@@ -3,12 +3,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { setWork } from './../redux/actions/work';
 import './../css/style.css';
 import { addWorks } from '../redux/actions/works';
+import { show } from './../redux/actions/showEvents';
 const handleSubmit = (e) => {
     e.preventDefault();
 };
 const Todo = () => {
     const work = useSelector((state) => state.work);
     const works = useSelector((state) => state.works);
+    const showEvent = useSelector((state) => state.showEvent);
     const dispatch = useDispatch();
     let count = 1;
     return (
@@ -18,6 +20,12 @@ const Todo = () => {
         >
             <div class="card bg-info text-white text-center mb-2">
                 <div class="card-body">Personal Manager</div>
+                <span>
+                    works:{' '}
+                    <span className="badge badge-pill bg-dark">
+                        {works.length}
+                    </span>
+                </span>
             </div>
             <div style={{ display: 'flex', justifyContent: 'center' }}>
                 <form class="form-inline" onSubmit={(e) => handleSubmit(e)}>
@@ -43,25 +51,36 @@ const Todo = () => {
                     </button>
                 </form>
             </div>
-            {works.map((w) => (
-                <div
-                    class="card bg-primary text-white text-center borderC"
-                    key={w.id}
+            {showEvent
+                ? works.map((w) => (
+                      <div
+                          class="card bg-primary text-white text-center borderC"
+                          key={w.id}
+                      >
+                          <div class="card-body ">
+                              <p style={{ float: 'right' }}>{`:${count++}`}</p>
+                              <p style={{ float: 'right' }}>{w.name}</p>
+                              <a>status</a>
+                              <i
+                                  className="fa fa-trash"
+                                  style={{
+                                      float: 'left',
+                                      fontSize: '25px',
+                                  }}
+                              ></i>
+                          </div>
+                      </div>
+                  ))
+                : null}
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <button
+                    className="btn btn-info mt-1"
+                    style={{ width: '25%' }}
+                    onClick={() => dispatch(show())}
                 >
-                    <div class="card-body ">
-                        <p style={{ float: 'right' }}>{`:${count++}`}</p>
-                        <p style={{ float: 'right' }}>{w.name}</p>
-                        <a>status</a>
-                        <i
-                            className="fa fa-trash"
-                            style={{
-                                float: 'left',
-                                fontSize: '25px',
-                            }}
-                        ></i>
-                    </div>
-                </div>
-            ))}
+                    show/hide
+                </button>
+            </div>
         </div>
     );
 };
