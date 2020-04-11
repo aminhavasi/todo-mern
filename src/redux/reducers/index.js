a890222f95716881a5a4c3bd1c1b4630631cddb7
amin havasi <pau.ahq@gmail.com>
2020-04-11 13:14:23 +0430

    reducers combiend

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,8 +2,10 @@ import { showEvent } from './showEvent';
 import { work } from './work';
 import { workReducer } from './works';
 import { combineReducers } from 'redux';
+import { radio } from './radio';
 export const reducers = combineReducers({
     showEvent,
     work,
+    radio,
     works: workReducer,
 });
